2744df337c6f3a06468c36aa967b8fbfc15352c1
Jacob Jones <jacob.j.jones01@gmail.com>
2023-01-17 11:49:54 -0600

    Got Post Controller implemented

--- src/main/java/com/codeup/blogapp/PostController.java
+++ src/main/java/com/codeup/blogapp/PostController.java
@@ -1,4 +1,34 @@
 package com.codeup.blogapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Handles all functions and mapping dealing with post, posting, viewing posts
+ */
+@Controller
 public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String getPosts(){
+        return "You're on the Posts page!";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPostNumber(@PathVariable int id){
+        return "You're on the posts page for post #" + id + "!";
+    }
+
+    @RequestMapping(path="/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String createPost_GET(){
+        return "You're on the create posts GET page!";
+    }
+
+    @RequestMapping(path="/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String createPost_POST(){
+        return "You're on the create posts POST page!";
+    }
 }
