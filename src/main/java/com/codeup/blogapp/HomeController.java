eba54147d1c97dc2c28f4b49259fff23b69364d6
Jacob Jones <jacob.j.jones01@gmail.com>
2023-01-17 12:12:04 -0600

    Added Thymeleaf support/ home.index

--- src/main/java/com/codeup/blogapp/HomeController.java
+++ src/main/java/com/codeup/blogapp/HomeController.java
@@ -2,14 +2,12 @@ package com.codeup.blogapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/home")
     public String hello(){
-        return "This is the landing page!";
+        return "home";
     }
 }
