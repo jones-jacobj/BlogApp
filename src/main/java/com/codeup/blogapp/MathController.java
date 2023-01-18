3abcd71d028c4cbb9ef4acc3bcb3860feca924e4
Jacob Jones <jacob.j.jones01@gmail.com>
2023-01-18 15:26:26 -0600

    Created posts

--- src/main/java/com/codeup/blogapp/MathController.java
+++ src/main/java/com/codeup/blogapp/MathController.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MathController {
-//    @RequestMapping(name="/math/add/{num1}/and/{num2}", method = RequestMethod.GET)
 @GetMapping("/math/add/{num1}/and/{num2}")
     @ResponseBody
     public String addition(@PathVariable int num1, @PathVariable int num2){
         return Integer.toString(num1 + num2);
     }
 
-//    @RequestMapping(name="/math/subtract/{num1}/from/{num2}", method = RequestMethod.GET)
 @GetMapping("/math/subtract/{num1}/from/{num2}")
     @ResponseBody
     public String subtract(@PathVariable int num1, @PathVariable int num2){
